b1e80efc2c178fdb929b61d013a7f8a1470372bc
rhtjddls123 <jjjk0605@naver.com>
2025-03-12 18:10:47 +0900

    feat: AuthButton 컴포넌트 분리

---
+++ src/components/AuthButton.tsx
@@ -0,0 +1,9 @@
+const AuthButton = () => {
+  return (
+    <button className="w-full px-4 py-3 rounded-lg bg-primary title-semi-18 text-gray-01">
+      로그인
+    </button>
+  );
+};
+
+export default AuthButton;
