f16c1b1fe296be9ddb9058102e4f224e7f372fe5
rhtjddls123 <jjjk0605@naver.com>
2025-03-23 19:43:25 +0900

    style: 프로필 및 지출 내역 정보 컴포넌트 스타일

--- src/features/main/ProfileOverview.tsx
+++ src/features/main/ProfileOverview.tsx
@@ -1,9 +1,11 @@
+import Overview from "./Overview";
 import Profile from "./Profile";
 
 const ProfileOverview = () => {
   return (
     <div className="m-6">
       <Profile />
+      <Overview />
     </div>
   );
 };
