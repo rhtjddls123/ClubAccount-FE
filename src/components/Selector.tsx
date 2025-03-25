567aea20298eef54c66c56b52e98a538d2fe2d3b
rhtjddls123 <jjjk0605@naver.com>
2025-03-25 15:24:16 +0900

    feat: Selector의 data를 내부에서 관리하도록 변경

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,14 +1,14 @@
-import { ReactNode, useState } from "react";
+import { InputHTMLAttributes, ReactNode, useState } from "react";
 import ArrowDownIcon from "../icons/ArrowDownIcon";
 
-interface SelectorProps<T> {
-  title: ReactNode;
-  list: T[];
-  onSelect: (data: T) => void;
+interface SelectorProps<T> extends InputHTMLAttributes<HTMLInputElement> {
+  selectTitle: ReactNode;
+  selectList: T[];
 }
 
-const Selector = <T,>({ title, list, onSelect }: SelectorProps<T>) => {
+const Selector = <T,>({ selectTitle, selectList, ...props }: SelectorProps<T>) => {
   const [open, setOpen] = useState(false);
+  const [data, setData] = useState<T>();
 
   let classes =
     "flex items-center justify-between w-full h-10 gap-1 px-4 border cursor-pointer rounded-t-xl border-gray-05 ";
@@ -20,7 +20,9 @@ const Selector = <T,>({ title, list, onSelect }: SelectorProps<T>) => {
   const handleToggle = () => {
     setOpen((pre) => !pre);
   };
-  const handleClose = () => {
+
+  const handleSelect = (select: T) => {
+    setData(select);
     setOpen(false);
   };
 
@@ -28,28 +30,26 @@ const Selector = <T,>({ title, list, onSelect }: SelectorProps<T>) => {
     <>
       <div className="relative">
         <button type="button" onClick={handleToggle} className={classes}>
-          <p className="body-med-14 text-gray-01">{title}</p>
+          <p className="body-med-14 text-gray-01">{data ? String(data) : selectTitle}</p>
           <ArrowDownIcon className="text-gray-01" />
         </button>
         {open && (
-          <ul className="absolute w-full border border-t-0 shadow-sm border-gray-05 rounded-b-xl">
-            {list.map((data, idx) => (
+          <ul className="absolute z-10 w-full border border-t-0 shadow-sm border-gray-05 rounded-b-xl">
+            {selectList.map((data, idx) => (
               <li key={idx}>
                 <button
-                  onClick={() => {
-                    onSelect(data);
-                    handleClose();
-                  }}
+                  onClick={() => handleSelect(data)}
                   className="w-full h-10 px-4 text-left bg-white body-med-14 text-gray-01"
                 >
                   {String(data)}
                 </button>
-                {list.length - 1 !== idx && <hr />}
+                {selectList.length - 1 !== idx && <hr />}
               </li>
             ))}
           </ul>
         )}
       </div>
+      <input className="hidden" value={String(data)} readOnly {...props} />
     </>
   );
 };
