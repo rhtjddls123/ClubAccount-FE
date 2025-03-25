75573b5155c4545916598987cb90f771604e917f
rhtjddls123 <jjjk0605@naver.com>
2025-03-25 15:28:22 +0900

    chore: react-tailwind-datepicker 설치

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,css}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,css}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.{js,ts}"
+  ],
   theme: {
     extend: {
       colors: {
