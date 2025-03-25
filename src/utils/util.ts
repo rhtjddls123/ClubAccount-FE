3b82ad0d1fe1f52420064c29219f882c20742deb
rhtjddls123 <jjjk0605@naver.com>
2025-03-25 15:28:41 +0900

    feat: date포맷팅 함수 구현

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,3 +1,7 @@
 export const formatAmount = (amount: number): string => {
   return amount.toLocaleString() + "원";
 };
+
+export const formatDate = (date: Date | null) => {
+  return date ? date.toISOString().split("T")[0] : "";
+};
